feeeac98e5f1901798c02c45ab75478776a44539
Buddhi Rangana <buddhirangana1999@gmail.com>
2025-11-12 00:35:18 +0530

    implement export data

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("com.android.library") version "8.13.0" apply false
+    id("com.android.library") version "8.13.1" apply false
     // Google services plugin (needed for Firebase)
     id("com.google.gms.google-services") version "4.4.4" apply false
 }
